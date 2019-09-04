56ab1d3f4c89ea7ffaddbae17153fff7659b715c
xiaohangzhan <xiaohangzhan@outlook.com>
2019-09-04 16:33:42 +0800

    update

--- scripts/run_det_detectron_pretrain.sh
+++ scripts/run_det_detectron_pretrain.sh
@@ -4,18 +4,9 @@ param_folder="/mnt/lustre/share/zhanxiaohang/data/fair_self_supervision_benchmar
 detectron="/mnt/lustre/share/zhanxiaohang/proj/detectron"
 exp="supervised_in1k/voc07"
 
-if true; then
-python extra_scripts/pickle_caffe2_detection.py \
-    --c2_model $param_folder/resnet50_in1k_supervised.pkl \
-    --output_file $param_folder/resnet50_in1k_supervised_detectron.pkl \
-    --absorb_std
-fi
-
-mkdir -p $data/detectron-output/$exp
-
 python $detectron/tools/train_net.py \
     --multi-gpu-testing \
     --cfg "configs/benchmark_tasks/object_detection_frozen/voc07/fast_rcnn_R-50-C4_with_ss_proposals_trainval.yaml" \
     OUTPUT_DIR $data/detectron-output/$exp \
-    TRAIN.WEIGHTS $param_folder/resnet50_in1k_supervised_detectron.pkl \
+    TRAIN.WEIGHTS /mnt/lustre/share/zhanxiaohang/data/detectron-download-cache/ImageNetPretrained/MSRA/resnet50_in1k_supervised.pkl \
     2>&1 | tee $data/detectron-output/$exp/log.txt
---
+++ scripts/run_faster_jigsaw_in1k.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+data="/mnt/lustre/share/zhanxiaohang/data/ssl-benchmark-output"
+param_folder="/mnt/lustre/share/zhanxiaohang/data/fair_self_supervision_benchmark_cache/models"
+detectron="/mnt/lustre/share/zhanxiaohang/proj/detectron"
+exp="faster_jigsaw_in1k/voc07"
+
+
+if false; then
+python extra_scripts/pickle_jigsaw_model.py \
+    --model_file $param_folder/resnet50_jigsaw_in1k_pretext.pkl \
+    --output_file $param_folder/resnet50_jigsaw_in1k_pretext_converted.pkl
+fi
+
+if false; then
+python extra_scripts/pickle_caffe2_detection.py \
+    --c2_model $param_folder/resnet50_jigsaw_in1k_pretext_converted.pkl \
+    --output_file $param_folder/resnet50_jigsaw_in1k_pretext_detectron.pkl \
+    --absorb_std
+fi
+
+mkdir -p $data/detectron-output/$exp
+
+python $detectron/tools/train_net.py \
+    --multi-gpu-testing \
+    --cfg "configs/benchmark_tasks/object_detection_frozen/voc07/e2e_faster_rcnn_R-50-C4_trainval.yaml" \
+    OUTPUT_DIR $data/detectron-output/$exp \
+    TRAIN.WEIGHTS $param_folder/resnet50_jigsaw_in1k_pretext_detectron.pkl \
+    2>&1 | tee $data/detectron-output/$exp/log.txt

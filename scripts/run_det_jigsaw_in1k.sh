728179c169a8809eaa91e7d93c1b208c73344e0b
xiaohangzhan <xiaohangzhan@outlook.com>
2019-09-05 11:26:24 +0800

    update

--- scripts/run_det_jigsaw_in1k.sh
+++ scripts/run_det_jigsaw_in1k.sh
@@ -5,18 +5,18 @@ detectron="/mnt/lustre/share/zhanxiaohang/proj/detectron"
 exp="jigsaw_in1k/voc07"
 
 
-if false; then
-python extra_scripts/pickle_jigsaw_model.py \
-    --model_file $param_folder/resnet50_jigsaw_in1k_pretext.pkl \
-    --output_file $param_folder/resnet50_jigsaw_in1k_pretext_converted.pkl
-fi
-
-if false; then
-python extra_scripts/pickle_caffe2_detection.py \
-    --c2_model $param_folder/resnet50_jigsaw_in1k_pretext_converted.pkl \
-    --output_file $param_folder/resnet50_jigsaw_in1k_pretext_detectron.pkl \
-    --absorb_std
-fi
+#if true; then
+#python extra_scripts/pickle_jigsaw_model.py \
+#    --model_file $param_folder/resnet50_jigsaw_in1k_pretext.pkl \
+#    --output_file $param_folder/resnet50_jigsaw_in1k_pretext_converted.pkl
+#fi
+#
+#if true; then
+#python extra_scripts/pickle_caffe2_detection.py \
+#    --c2_model $param_folder/resnet50_jigsaw_in1k_pretext_converted.pkl \
+#    --output_file $param_folder/resnet50_jigsaw_in1k_pretext_detectron.pkl \
+#    --absorb_std
+#fi
 
 mkdir -p $data/detectron-output/$exp
 
@@ -24,5 +24,5 @@ python $detectron/tools/train_net.py \
     --multi-gpu-testing \
     --cfg "configs/benchmark_tasks/object_detection_frozen/voc07/fast_rcnn_R-50-C4_with_ss_proposals_trainval.yaml" \
     OUTPUT_DIR $data/detectron-output/$exp \
-    TRAIN.WEIGHTS $param_folder/resnet50_jigsaw_in1k_pretext_detectron.pkl \
+    TRAIN.WEIGHTS $param_folder/resnet50_jigsaw_in1k_pretext.pkl \
     2>&1 | tee $data/detectron-output/$exp/log.txt
